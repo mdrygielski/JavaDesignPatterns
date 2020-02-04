cf35393aa5b3dac80bedf66390ab76fa1d45528d
Marcin <lysy620@gmail.com>
2020-02-04 20:50:41 +0100

    dev1

--- src/main/java/com/giel/Application.java
+++ src/main/java/com/giel/Application.java
@@ -60,7 +60,7 @@ public class Application {
     public static void main(String[] args) {
         Application app = new Application();
 
-        // cleanup
+        // dav changes
 
         // Creational design patterns
 //        app.SingletonExample();
