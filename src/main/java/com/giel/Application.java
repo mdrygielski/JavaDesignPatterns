5c558d366392f21b6643877227bfb78565aef47b
Marcin <lysy620@gmail.com>
2020-02-04 21:37:44 +0100

    1+2

--- src/main/java/com/giel/Application.java
+++ src/main/java/com/giel/Application.java
@@ -59,7 +59,7 @@ public class Application {
 
     public static void main(String[] args) {
         Application app = new Application();
-
+12
         // dav changes
         // cleanup
         //hotfix
